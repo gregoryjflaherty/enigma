687e0cbc9dcf6266eeccdb30e3e54d7a1768b4c3
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 17:37:21 -0500

    Fix: edit error in test format

--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require './lib/date'
-RSpec.describe do Key
+RSpec.describe do Date
 
   context 'setup' do
     let(:day) {Date.new('040289')}
@@ -9,4 +9,4 @@ RSpec.describe do Key
       expect(day).to be_a(Date)
     end
   end
-end 
+end
