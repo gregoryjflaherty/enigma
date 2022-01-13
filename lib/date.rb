4d742c3562a6558c523812b41db58d875b20f1e4
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 18:02:05 -0500

    Feat: add initialize, get_last_four method and a_shift attr

--- lib/date.rb
+++ lib/date.rb
@@ -1,3 +1,17 @@
 class Date
+  attr_reader :last_four,
+              :a_shift,
+              :b_shift,
+              :c_shift,
+              :d_shift
 
-end 
+  def initialize(date)
+    @last_four = get_last_four(date)
+    @a_shift = @last_four[0].to_i
+  end
+
+  def get_last_four(date)
+    large_num = date.to_i ** 2
+    large_num.to_s[-4..-1]
+  end
+end
