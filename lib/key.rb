05689fd64a90a924f6e776031c9df9eb7caf5c99
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 17:32:54 -0500

    Feat: create d_key attr

--- lib/key.rb
+++ lib/key.rb
@@ -2,12 +2,14 @@ class Key
   attr_reader :all_digits,
               :a_key,
               :b_key,
-              :c_key
+              :c_key,
+              :d_key
 
   def initialize(all_digits)
     @all_digits = all_digits
     @a_key = all_digits[0..1].to_i
     @b_key = all_digits[1..2].to_i
     @c_key = all_digits[2..3].to_i
+    @d_key = all_digits[3..4].to_i
   end
 end
