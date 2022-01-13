70991c9914391a62805fcf3faa80d1a47315ae94
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 17:29:37 -0500

    Feat: create b_key attr

--- lib/key.rb
+++ lib/key.rb
@@ -1,9 +1,11 @@
 class Key
-  attr_reader :all_digits, :a_key
+  attr_reader :all_digits,
+              :a_key,
+              :b_key
 
   def initialize(all_digits)
     @all_digits = all_digits
     @a_key = all_digits[0..1].to_i
+    @b_key = all_digits[1..2].to_i
   end
 end
-require "pry"; binding.pry
