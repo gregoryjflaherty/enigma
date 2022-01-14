4252c1888bccaacb0f43e1923dc49011ad4482bd
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-14 12:12:31 -0500

    Feat: add to change method to account for symbols - needs refactor

--- lib/cryptography.rb
+++ lib/cryptography.rb
@@ -20,4 +20,24 @@ module Cryptography
     end
     new_message.join
   end
+
+  def change(letters, message, shift_array)
+    new_message = []
+    symbols = 0
+    message.split("").each_with_index do |letter, index|
+      if !letters.include?(letter)
+        new_message << letter
+        symbols += 1
+        next
+      end
+      index -= symbols
+      index > 3 ? new_index = index % 4 : new_index = index
+      new_message << rotate(letters, letter, shift_array[new_index])
+    end
+    new_message.join
+  end
+
+  # def ignore_symbols(letter, index)
+  #
+  # end
 end
