7064717330c5d9a690408ece10a66ed2a66fd5e4
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 21:10:29 -0500

    Test: create exists and letters test

---
+++ spec/shift_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/shift'
+RSpec.describe do Shift
+
+  context 'setup' do
+    let(:shift) {Shift.new}
+
+    it 'exists' do
+      expect(shift).to be_a(Shift)
+    end
+
+    it 'has letters' do
+      letters = ("a".."z").to_a << " "
+      expect(shift.letters).to eq(letters)
+  end
+end
