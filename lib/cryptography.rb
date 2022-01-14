ee85a810d4e66d9138c8c5b49b6b90f7bbb4aa90
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 22:29:35 -0500

    Feat: create rotate method - all tests passing

--- lib/cryptography.rb
+++ lib/cryptography.rb
@@ -2,4 +2,16 @@ module Cryptography
   def once_around(shift)
     shift > 27 ? shift % 27 : shift
   end
+
+  def rotate(letters, single_letter, shift)
+    letters.each_with_index do |letter, index|
+      if letter == single_letter
+        if (index + shift) > 27
+          return letters[once_around(index + shift)]
+        else
+          return letters[index + shift]
+        end
+      end
+    end
+  end
 end
