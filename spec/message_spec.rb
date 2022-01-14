0b296a8a0e7701fe9b501ad2a2d70087e03c2b7e
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 20:51:15 -0500

    Test: create text input test

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe do Message
     it 'exists' do
       expect(message).to be_a(Message)
     end
+
+    it 'has text input' do
+      expect(message.text_input).to eq('Hello World')
+    end
   end
 end
