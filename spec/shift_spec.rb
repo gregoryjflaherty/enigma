f613fbefc21871d6b0ce6a74ed9155ad4f6c132a
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-13 21:21:54 -0500

    Test: create key and offset test

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -1,9 +1,11 @@
 require 'rspec'
 require './lib/shift'
+require './lib/key'
+require './lib/date'
 RSpec.describe do Shift
 
   context 'setup' do
-    let(:shift) {Shift.new}
+    let(:shift) {Shift.new('01234', '040289')}
 
     it 'exists' do
       expect(shift).to be_a(Shift)
@@ -12,6 +14,16 @@ RSpec.describe do Shift
     it 'has letters' do
       letters = ("a".."z").to_a << " "
       expect(shift.letters).to eq(letters)
-    end 
+    end
+
+    it 'has a key' do
+      key1 = Key.new('01234')
+      expect(shift.key).to eq(key1)
+    end
+
+    it 'has an offset' do
+      offset = Date.new('040289')
+      expect(shift.offset).to eq(offset)
+    end
   end
 end
