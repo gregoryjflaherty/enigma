ddadf952f61c441d90f74900e35af52509202e83
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-15 11:41:16 -0500

    Refactor: remove unnecessary argument from all methods - change to attr

--- lib/cryptography.rb
+++ lib/cryptography.rb
@@ -3,32 +3,32 @@ module Cryptography
     shift > 27 ? shift % 27 : shift
   end
 
-  def rotate(letters, single_letter, shift)
-    letters.each_with_index do |letter, index|
+  def rotate(single_letter, shift)
+    @letters.each_with_index do |letter, index|
       new_shift = index + shift
       if letter == single_letter
-        return letters[once_around(new_shift)]
+        return @letters[once_around(new_shift)]
       end
     end
   end
 
-  def change(letters, message, shift_array)
+  def change(message, shift_array)
     new_message = []
     symbols = 0
     message.split("").each_with_index do |letter, index|
-      if !letters.include?(letter)
+      if !@letters.include?(letter)
         new_message << letter
         symbols += 1
         next
       end
       index -= symbols
-      add_changed_letters(new_message, index, letter, letters, shift_array)
+      add_changed_letters(new_message, index, letter, shift_array)
     end
     new_message.join
   end
 
-  def add_changed_letters(new_message, index, letter, letters, shift_array)
+  def add_changed_letters(new_message, index, letter, shift_array)
     index > 3 ? new_index = index % 4 : new_index = index
-    new_message << rotate(letters, letter, shift_array[new_index])
+    new_message << rotate(letter, shift_array[new_index])
   end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -1,8 +1,7 @@
 require 'rspec'
 require './lib/shift'
-require './lib/key'
-require './lib/offset'
 require './lib/producer'
+
 RSpec.describe do Shift
 
   context 'setup' do
@@ -33,50 +32,50 @@ RSpec.describe do Shift
       expect(shift.produce_offset_set('040289')).to eq([3, 5, 2, 1])
     end
 
-    xit 'has a shift array' do
+    it 'has a shift array' do
       expect(shift.shift_array).to eq([4, 17, 25, 8])
     end
 
-    xit 'rotates' do
+    it 'rotates' do
       @letters = ("a".."z").to_a << " "
       letter = 'a'
       shift_1 = 1
-      expect(shift.rotate(@letters, letter, shift_1)).to eq('b')
+      expect(shift.rotate(letter, shift_1)).to eq('b')
     end
 
-    xit 'rotates bigly' do
+    it 'rotates bigly' do
       @letters = ("a".."z").to_a << " "
       letter = 'j'
       shift_1 = 25
-      expect(shift.rotate(@letters, letter, shift_1)).to eq('h')
+      expect(shift.rotate(letter, shift_1)).to eq('h')
     end
 
-    xit 'changes message' do
+    it 'changes message' do
       @letters = ("a".."z").to_a << " "
       message = 'abcd'
       shift_array = [1, 1, 1, 1]
-      expect(shift.change(@letters, message, shift_array)).to eq('bcde')
+      expect(shift.change(message, shift_array)).to eq('bcde')
     end
 
-    xit 'changes message bigly' do
+    it 'changes message bigly' do
       @letters = ("a".."z").to_a << " "
       message = 'hello world'
       shift_array = [4, 17, 25, 8]
-      expect(shift.change(@letters, message, shift_array)).to eq('lvjtsquwvbb')
+      expect(shift.change( message, shift_array)).to eq('lvjtsquwvbb')
     end
 
-    xit 'ignores symbols' do
+    it 'ignores symbols' do
       @letters = ("a".."z").to_a << " "
       message = '!hello world!'
       shift_array = [4, 17, 25, 8]
-      expect(shift.change(@letters, message, shift_array)).to eq('!lvjtsquwvbb!')
+      expect(shift.change( message, shift_array)).to eq('!lvjtsquwvbb!')
     end
 
-    xit 'ignores symbols bigly' do
+    it 'ignores symbols bigly' do
       @letters = ("a".."z").to_a << " "
       message = '!@#$hi$#@!'
       shift_array = [4, 17, 25, 8]
-      expect(shift.change(@letters, message, shift_array)).to eq('!@#$lz$#@!')
+      expect(shift.change(message, shift_array)).to eq('!@#$lz$#@!')
     end
   end
 end
