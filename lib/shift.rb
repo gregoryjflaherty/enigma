7e4908d8c6ede946b709be6f5aa179c316334e5c
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-15 11:27:47 -0500

    Refactor: edit shift class to fit producer module

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,24 +1,20 @@
 require './lib/key'
 require './lib/offset'
 require './lib/cryptography'
+require './lib/producer'
 
 class Shift
   include Cryptography
+  include Producer
   attr_reader :letters,
               :key,
-              :offset,
-              :a_shift,
-              :b_shift,
-              :c_shift,
-              :d_shift
+              :offset
+              :shift_array
 
   def initialize(key, date)
     @letters = ("a".."z").to_a << " "
-    @key = Key.new(key)
-    @offset = Date.new(date)
-    @a_shift = once_around(@key.a_key + @offset.a_offset)
-    @b_shift = once_around(@key.b_key + @offset.b_offset)
-    @c_shift = once_around(@key.c_key + @offset.c_offset)
-    @d_shift = once_around(@key.d_key + @offset.d_offset)
+    @key = key
+    @offset = date
+    @shift_array = produce_shift_array(key, date)
   end
 end
