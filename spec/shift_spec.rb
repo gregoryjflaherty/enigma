17ec9812308e42186e09e89a6ad1c39155b4ef43
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-15 11:11:23 -0500

    Test: create test for produce_key_set

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require './lib/shift'
 require './lib/key'
 require './lib/offset'
+require './lib/producer'
 RSpec.describe do Shift
 
   context 'setup' do
@@ -24,46 +25,50 @@ RSpec.describe do Shift
       expect(shift.offset).to eq('040289')
     end
 
-    it 'has a shift array' do
-      expect(shift.shift_array).to eq([[4, 17, 25, 8]])
+    it 'produces_key_set' do
+      expect(shift.produce_key_set('01234')).to eq([01, 12, 23, 34])
     end
 
-    it 'rotates' do
+    xit 'has a shift array' do
+      expect(shift.shift_array).to eq([4, 17, 25, 8])
+    end
+
+    xit 'rotates' do
       @letters = ("a".."z").to_a << " "
       letter = 'a'
       shift_1 = 1
       expect(shift.rotate(@letters, letter, shift_1)).to eq('b')
     end
 
-    it 'rotates bigly' do
+    xit 'rotates bigly' do
       @letters = ("a".."z").to_a << " "
       letter = 'j'
       shift_1 = 25
       expect(shift.rotate(@letters, letter, shift_1)).to eq('h')
     end
 
-    it 'changes message' do
+    xit 'changes message' do
       @letters = ("a".."z").to_a << " "
       message = 'abcd'
       shift_array = [1, 1, 1, 1]
       expect(shift.change(@letters, message, shift_array)).to eq('bcde')
     end
 
-    it 'changes message bigly' do
+    xit 'changes message bigly' do
       @letters = ("a".."z").to_a << " "
       message = 'hello world'
       shift_array = [4, 17, 25, 8]
       expect(shift.change(@letters, message, shift_array)).to eq('lvjtsquwvbb')
     end
 
-    it 'ignores symbols' do
+    xit 'ignores symbols' do
       @letters = ("a".."z").to_a << " "
       message = '!hello world!'
       shift_array = [4, 17, 25, 8]
       expect(shift.change(@letters, message, shift_array)).to eq('!lvjtsquwvbb!')
     end
 
-    it 'ignores symbols bigly' do
+    xit 'ignores symbols bigly' do
       @letters = ("a".."z").to_a << " "
       message = '!@#$hi$#@!'
       shift_array = [4, 17, 25, 8]
