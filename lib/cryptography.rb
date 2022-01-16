ff24d7e9e2a0e7427ce67dc15b84ab820ec46981
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-16 10:36:36 -0500

    Refactor: edit rotate method to include decrpyt

--- lib/cryptography.rb
+++ lib/cryptography.rb
@@ -3,16 +3,7 @@ module Cryptography
     shift > 27 ? shift % 27 : shift
   end
 
-  def rotate(single_letter, shift)
-    @letters.each_with_index do |letter, index|
-      new_shift = index + shift
-      if letter == single_letter
-        return @letters[once_around(new_shift)]
-      end
-    end
-  end
-
-  def change(message, shift_array)
+  def change(encrypt_decrypt, message, shift_array)
     new_message = []
     symbols = 0
     message.split("").each_with_index do |letter, index|
@@ -22,13 +13,23 @@ module Cryptography
         next
       end
       index -= symbols
-      add_changed_letters(new_message, index, letter, shift_array)
+      add_changed_letters(encrypt_decrypt, new_message, index, letter, shift_array)
     end
     new_message.join
   end
 
-  def add_changed_letters(new_message, index, letter, shift_array)
+  def add_changed_letters(encrypt_decrypt, new_message, index, letter, shift_array)
     index > 3 ? new_index = index % 4 : new_index = index
-    new_message << rotate(letter, shift_array[new_index])
+    new_message << rotate(encrypt_decrypt, letter, shift_array[new_index])
+  end
+
+  def rotate(encrypt_decrypt, single_letter, shift)
+    @letters.each_with_index do |letter, index|
+      new_shift = (index + shift) if encrypt_decrypt == 'encrypt'
+      new_shift = (index - shift) if encrypt_decrypt == 'decrypt'
+      if letter == single_letter
+        return @letters[once_around(new_shift)]
+      end
+    end
   end
 end
