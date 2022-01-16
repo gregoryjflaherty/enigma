d43ba5c74c498f26567686b96e95a20f0851404f
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-16 10:47:05 -0500

    Feat: create encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,19 @@
+require './lib/shift'
+
 class Enigma
+
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
     @shift = Shift.new(@key, @date)
+  end
+
+  def encrypt(message, key, date)
+    hash_output = {
+      :encryption => @shift.change('encrypt', message, @shift.shift_array),
+      :key => key,
+      :date => date
+    }
   end
 end
