11034f5794cacf9a8200e38d861a963d1cfae40f
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-16 23:30:04 -0500

    Feat: create encrypt message method

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,16 @@
-class Message
-  attr_reader :text_input
+require './lib/encrypt'
+require './lib/enigma'
 
-  def initialize(text_input)
-    @text_input = text_input
+class Message
+
+  def self.encrypt_message(message)
+    text = message.read
+    message.close
+    machine = Enigma.new
+    encrypted_message = machine.encrypt(text.strip)
+    output = File.open(ARGV[1], 'w')
+    output.write(encrypted_message[:encryption])
+    puts "Created '#{ARGV[1].to_s}' with the key #{encrypted_message[:key]} and the date #{encrypted_message[:date]}"
   end
 end
+Message.encrypt_message(File.open(ARGV[0], 'r'))
