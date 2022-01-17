4aa79bd0b435c3267862efad9d0221a1bff11869
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-17 00:51:40 -0500

    Feat: add decrypt method

--- lib/message.rb
+++ lib/message.rb
@@ -1,8 +1,6 @@
-require './lib/encrypt'
 require './lib/enigma'
 
 class Message
-
   def self.encrypt_message(message)
     text = message.read
     message.close
@@ -12,5 +10,14 @@ class Message
     output.write(encrypted_message[:encryption])
     puts "Created '#{ARGV[1].to_s}' with the key #{encrypted_message[:key]} and the date #{encrypted_message[:date]}"
   end
+
+  def self.decrypt_message(message)
+    text = message.read
+    message.close
+    machine = Enigma.new
+    decrypted_message = machine.decrypt(text.strip, ARGV[2], ARGV[3])
+    output = File.open(ARGV[1], 'w')
+    output.write(decrypted_message[:decryption])
+    puts "Created '#{ARGV[1].to_s}' with the key #{decrypted_message[:key]} and the date #{decrypted_message[:date]}"
+  end
 end
-Message.encrypt_message(File.open(ARGV[0], 'r'))
